5a770e2af5d77eb3e0d361d338b659ada7d86d4e
이경환 <111859180+kyeonghwanlee@users.noreply.github.com>
2022-10-17 15:56:48 +0900

    Add files via upload

---
+++ cypress/e2e/3_mypage_editinformation.cy.js
@@ -0,0 +1,110 @@
+describe('empty spec', () => {
+
+  beforeEach(function () {
+    cy.viewport(1920,1080)
+    cy.visit('https://sso.liveklass.com/login')
+
+     // id & pw text input
+     cy.get('.form_group > .lk-input > .lk-input-inner > input').type('pogap61665@otodir.com')
+     cy.get('[style="margin-bottom: 1.25rem;"] > .lk-input > .lk-input-inner > input').type('@dl29240730')
+ 
+     // login button click
+     cy.get('.lk-button').click()
+     cy.wait(1000)
+  })
+
+
+  it('3. mypage page check', () => {
+    //button check
+    cy.get(':nth-child(1) > .header-menu-item-btn').contains('운영 가이드').should('exist') 
+    cy.get(':nth-child(2) > .header-menu-item-btn').contains('수강생 가이드').should('exist') 
+    cy.get(':nth-child(3) > .header-menu-item-btn').contains('라클아카데미').should('exist') 
+    
+    // user stting button check
+    cy.get('.ki-user').click()
+    cy.wait(1000)
+
+    cy.get(':nth-child(3) > [href="/editinformation"]').click()
+    cy.wait(1000)
+    cy.url().should('include','/editinformation')    
+    cy.url().then((valse) => {
+      cy.log('The current real URL id: ', valse)
+    })
+  })
+
+  it('3-1. editinformation page check', () => {
+    cy.visit('https://sso.liveklass.com/editinformation')
+    //phone chage
+    cy.get(':nth-child(6) > .content_td > .form_control').clear()
+    cy.get(':nth-child(6) > .content_td > .form_control').type('01012345678')
+    cy.get('.button_container > .btn_common').click()
+    cy.get('.popup_b > .btn_common').click() 
+
+    //password chage
+    cy.get(':nth-child(1) > .content_td > .form_control').type('12')
+    cy.get('.button_container > .btn_common').click()
+    cy.wait(300)
+
+    cy.get(':nth-child(1) > .content_td > .form_control').clear()
+    cy.get(':nth-child(1) > .content_td > .form_control').type('@dl292407')
+    cy.get(':nth-child(2) > .content_td > .form_control').type('@dsfjl')
+    cy.wait(300)
+
+    cy.get(':nth-child(2) > .content_td > .form_control').clear()
+    cy.get(':nth-child(2) > .content_td > .form_control').type('@dl29240730') 
+    cy.get(':nth-child(7) > :nth-child(3) > .content_td > .form_control').type('@dsfjl')
+    cy.wait(300)
+
+    cy.get(':nth-child(7) > :nth-child(3) > .content_td > .form_control').clear()
+    cy.get(':nth-child(7) > :nth-child(3) > .content_td > .form_control').type('@dl29240730')
+
+    cy.get('.button_container > .btn_common').click() 
+    cy.get('.popup_b > .btn_common').click()
+    cy.wait(300)
+
+    cy.get(':nth-child(1) > .content_td > .form_control').clear()
+    cy.get(':nth-child(1) > .content_td > .form_control').type('@dl29240730')
+    cy.get(':nth-child(2) > .content_td > .form_control').click()
+    cy.wait(300)
+
+    cy.get('.button_container > .btn_common').click() 
+    cy.get('.popup_b > .btn_common').click()
+
+    //paymenthistory
+    cy.get('.settings_side_content > [href="/paymenthistory"]').click()
+    cy.wait(1000)
+    cy.url().should('include','/paymenthistory')    
+    cy.url().then((valse) => {
+      cy.log('The current real URL id: ', valse)
+    }) 
+
+    // user stting cilck
+    cy.get('.ki-user').click()
+
+    // logout
+    cy.get(':nth-child(4) > .lhp-menu-item').click()    
+  })
+
+  it('3-2. editinformation email check', () => {
+    cy.visit('https://sso.liveklass.com/editinformation')
+    cy.wait(200)
+
+    cy.get('.content_td > div > .btn_common').click()
+
+    cy.get('.mng-input-button').should('exist')  
+    cy.contains('계정 인증 완료').should('exist')  
+
+    cy.get('.mng-input-button').click()
+
+    cy.get(':nth-child(3) > .lk-mng-input > input').type('324789')
+    cy.contains('인증 코드가 잘못 되었습니다.').should('not.exist') 
+    cy.get('.lk-mng-modal-footer > .lk-mng-button').click()
+    cy.contains('인증 코드가 잘못 되었습니다.').should('exist') 
+    cy.wait(200)
+
+    cy.get('[class="btn_common btn_point btn_popup_small focusable"]').click()
+    cy.wait(200)
+    cy.get('.lk-mng-modal-header > .lk-mng-icon > img').click()
+
+  })
+})
---
+++ cypress/e2e/5_class_insert.cy.js
@@ -0,0 +1,115 @@
+const { verify } = require("crypto")
+
+describe('empty spec', () => {
+
+  beforeEach(function () {
+    cy.viewport(1920,1080)
+    cy.visit('https://testlee001.liveklass.com/')
+    cy.wait(100)
+
+    cy.get('.header-action > .outlined > span').click()
+    //cy.contains('로그인').click()
+    cy.wait(500)
+
+    // id & pw text input
+    cy.get('[type="text"]').clear()
+    cy.get('[type="password"]').clear()
+    cy.get('.form_group > .lk-input > .lk-input-inner > input').type('kyeonghwan.lee@liveklass.com')
+    cy.get('[style="margin-bottom: 1.25rem;"] > .lk-input > .lk-input-inner > input').type('@Dl292407')
+    cy.wait(500)
+    cy.get('.lk-button').click()
+    cy.wait(500)
+  })
+
+  it('5-1. class insert(강의 라이브 생성))', () => {
+    //프로그램 이동
+    cy.get(':nth-child(2) > .header-menu-item-btn').click()
+    cy.wait(100)
+
+    cy.get('[href="/classes/create"] > span').click()
+    cy.wait(100)
+
+    cy.contains('다음').click({waitForAnimations: false})
+    cy.contains('다음').click({waitForAnimations: false})
+    cy.get('[data-v-54f9a632]').contains('완료').click({force: true})
+    cy.wait(1000)
+
+    //cy.get('.lk-input-inner > input').clear()
+    cy.get('[placeholder="이름을 입력해 주세요"]').type('cypress insert test',{force: true})
+
+    cy.get('.class-section > :nth-child(1) > .lk-radio-group > :nth-child(2) > span').click()
+    cy.get('.class-section > :nth-child(1) > .lk-radio-group > :nth-child(3) > span').click()
+    cy.get('.lk-radio-group > :nth-child(4) > span').click()
+    cy.get('.lk-radio-group > :nth-child(1) > span').click()
+
+    //날짜 조정??
+    //cy.get(':nth-child(1) > .el-date-editor > .el-input__inner').click()
+    //cy.get('.el-icon-arrow-right').click() //월 이동 ->
+    //cy.get('.el-icon-arrow-left').click()  //월 이동 <-
+    //cy.get('.el-icon-d-arrow-right').click() //년 이동 ->
+    //cy.get('.el-icon-d-arrow-left').click()  //년 이동 <-
+    //cy.get(':nth-child(6) > :nth-child(6) > div > span').click() //일 선책
+    //앞의 :nth-child 내용은 행
+    //뒤의 :nth-child 는 열 (일1,월2,화3,수4,목5,금6,토7)
+
+    //시간 조정
+    //cy.get(':nth-child(1) > .timepicker-custom > .vue__time-picker > .display-time').click()
+    //cy.get(':nth-child(1) > .timepicker-custom > .vue__time-picker > .dropdown > .select-list > .hours > [data-key="14"]').click()
+    //cy.get(':nth-child(1) > .timepicker-custom > .vue__time-picker > .dropdown > .select-list > .minutes > [data-key="00"]').click()
+  
+    cy.get('.createBtn > .btn-label > span').click({force: true})
+  })
+
+  it('5-2. class insert(강의 라이브 강의 참여하기-강의실 입장))', () => {
+    //프로그램 이동
+    cy.get(':nth-child(2) > .header-menu-item-btn').click({force: true})
+    cy.wait(500)
+
+    cy.get('.swiper-slide-next > a').click({force: true})
+    cy.wait(500)
+
+    cy.get(':nth-child(1) > .lk-card-content > .lk-card-title').contains('cypress insert test').click({force: true})
+    cy.wait(500)
+    
+    cy.contains('강의실 입장').click({force: true})
+    cy.wait(500)
+
+    //cy.get('.classroom-button-wrapper > .lk-button > .btn-label > span').click({force: true})
+    //cy.go('back')
+  })
+
+  it('5-3. class insert(강의 라이브 강의 참여하기-팝업))', () => {
+    //cy.get('.live-item-container > [data-v-3a53cfd8=""] > .notification-card > .information-section > .join-button > .btn-label > span').click({force: true})
+    //cy.wait(500)
+    //cy.go('back')
+  })
+
+  it('5-4. class insert(강의 라이브 강의 참여하기-나의 강의실))', () => {
+    cy.get(':nth-child(4) > .header-menu-item-btn').click()
+    cy.wait(500)
+
+    cy.get('.mt30 > .close-button').click({force: true})
+    //cy.get('.lk-card-enter > .lk-btn > :nth-child(1)').click()
+    //cy.wait(500)
+    //cy.go('back')
+  })
+
+  it('5-5. class insert(강의 공개)', () => {
+    //프로그램 이동
+    cy.get(':nth-child(2) > .header-menu-item-btn').click()
+    cy.wait(500)
+
+    cy.get('.swiper-slide-next > a').click()
+    cy.wait(500)
+
+    cy.get(':nth-child(1) > .lk-card-content > .lk-card-title').contains('cypress insert test').click({force: true})
+    cy.wait(500)
+
+    cy.get('#view-step2 > .cv-edit-btn > .btn-label > span').click()
+    cy.wait(500)
+    cy.get('.el-switch__core').click()
+    cy.wait(500)
+    cy.get('.lk-modal-footer > .primary > .btn-label > span').click()
+  })
+
+})
