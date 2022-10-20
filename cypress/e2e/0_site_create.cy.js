65d9ce0a8f97b090c568704d55d7974f31055b38
kyeonghwan.lee <kyeonghwan.lee@liveklass.com>
2022-10-20 10:36:17 +0900

    site create commit7

--- cypress/e2e/0_site_create.cy.js
+++ cypress/e2e/0_site_create.cy.js
@@ -28,7 +28,7 @@ describe('empty spec', () => {
         cy.wait(500)
 
         cy.contains('올바른 이메일 형식으로 입력해 주세요.').should('exist') 
-        cy.contains('필수 정보입니다.').should('exist')
+        cy.contains('Required').should('exist')
         cy.contains('올바른 전화번호 형식으로 입력해 주세요').should('exist') 
 
         //로그인
