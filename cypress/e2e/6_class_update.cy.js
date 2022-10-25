e40b74a12be2665c52d602667c7236ddea077263
kyeonghwan.lee <kyeonghwan.lee@liveklass.com>
2022-10-25 13:43:06 +0900

    class update Test2

--- cypress/e2e/6_class_update.cy.js
+++ cypress/e2e/6_class_update.cy.js
@@ -21,31 +21,49 @@ describe('empty spec', () => {
     cy.wait(500)
   })
 
-  it('6-1. class update', () => {
+  it.only('6-1. class update', () => {
     //프로그램 이동
     cy.get(':nth-child(2) > .header-menu-item-btn').click()
     cy.wait(500)
-    cy.get('.active > a').contains('전체').should('exist')
-    cy.get('.swiper-slide-next > a').contains('비공개').should('exist')
+    if( i == 0 ){
+      cy.get('.active > a').contains('전체').should('exist')
+      cy.get('.swiper-slide-next > a').contains('비공개').should('exist')
+    }else{
+      cy.get('.active > a').contains('All').should('exist')
+      cy.get('.swiper-slide-next > a').contains('Private').should('exist')
+    }
 
     //강의실 1개 입장
     cy.get(':nth-child(1) > .lk-card-img > .lk-card-link').click()
 
     //check
-    cy.get(':nth-child(1) > .header-menu-item-btn').contains('소개').should('exist')
-    cy.get(':nth-child(2) > .header-menu-item-btn').contains('프로그램').should('exist')
-    cy.get(':nth-child(3) > .header-menu-item-btn').contains('공지사항').should('exist')
-    cy.get(':nth-child(4) > .header-menu-item-btn').contains('나의 강의실').should('exist')
-    cy.get('.el-tooltip > span').contains('사이트 관리').should('exist')
-    cy.get('.detail-btns > .btn-label > span').contains('찜하기').should('exist')
-    cy.get('.share-btns > .btn-label > span').contains('공유').should('exist')
-    cy.get('#view-step1 > .lk-button > .btn-label > span').contains('강의실 입장').should('exist')
-    cy.get('.gray > .btn-label > span').contains('클래스 복사').should('exist')
-    cy.get('.warning > .btn-label > span').contains('클래스 삭제').should('exist')
-    cy.get('#view-step2 > .cv-edit-btn > .btn-label > span').contains('클래스 관리').should('exist')
-    cy.get(':nth-child(4) > .class-description-wrapper > .class-description-title > .title').contains('커리큘럼').should('exist')
-    cy.get(':nth-child(6) > .class-description-wrapper > .class-description-title > .title').contains('질문 답변').should('exist')
-    cy.get('.detail-wrapper > :nth-child(2)').contains('1회 라이브').should('exist')
+    if( i == 0 ){
+      cy.get(':nth-child(1) > .header-menu-item-btn').contains('소개').should('exist')
+      cy.get(':nth-child(2) > .header-menu-item-btn').contains('프로그램').should('exist')
+      cy.get(':nth-child(3) > .header-menu-item-btn').contains('공지사항').should('exist')
+      cy.get(':nth-child(4) > .header-menu-item-btn').contains('나의 강의실').should('exist')
+      cy.get('.el-tooltip > span').contains('사이트 관리').should('exist')
+      cy.get('.detail-btns > .btn-label > span').contains('찜하기').should('exist')
+      cy.get('.share-btns > .btn-label > span').contains('공유').should('exist')
+      cy.get('#view-step1 > .lk-button > .btn-label > span').contains('강의실 입장').should('exist')
+      cy.get('.gray > .btn-label > span').contains('클래스 복사').should('exist')
+      cy.get('.warning > .btn-label > span').contains('클래스 삭제').should('exist')
+      cy.get('#view-step2 > .cv-edit-btn > .btn-label > span').contains('클래스 관리').should('exist')
+      cy.get(':nth-child(6) > .class-description-wrapper > .class-description-title > .title').contains('질문 답변').should('exist')
+    }else{
+      cy.get(':nth-child(1) > .header-menu-item-btn').contains('소개').should('exist')
+      cy.get(':nth-child(2) > .header-menu-item-btn').contains('프로그램').should('exist')
+      cy.get(':nth-child(3) > .header-menu-item-btn').contains('공지사항').should('exist')
+      cy.get(':nth-child(4) > .header-menu-item-btn').contains('나의 강의실').should('exist')
+      cy.get('.el-tooltip > span').contains('사이트 관리').should('exist')
+      cy.get('.detail-btns > .btn-label > span').contains('Like').should('exist')
+      cy.get('.share-btns > .btn-label > span').contains('Share').should('exist')
+      cy.get('#view-step1 > .lk-button > .btn-label > span').contains('Learn').should('exist')
+      cy.get('.gray > .btn-label > span').contains('클래스 복사').should('exist')
+      cy.get('.warning > .btn-label > span').contains('Delete Class').should('exist')
+      cy.get('#view-step2 > .cv-edit-btn > .btn-label > span').contains('클래스 관리').should('exist')
+      cy.get(':nth-child(6) > .class-description-wrapper > .class-description-title > .title').contains('Q & A').should('exist')
+    }
     cy.wait(500)
   })
 
@@ -58,19 +76,31 @@ describe('empty spec', () => {
     //클래스 관리 이동
     cy.get('#view-step2 > .cv-edit-btn > .btn-label > span').click()
     cy.wait(500)
+    
     //check
-    cy.get('.btn-move > .btn-label > span').contains('클래스로 돌아가기').should('exist')
-    cy.get('[style="font-size: 16px; color: rgb(17, 42, 68); margin-right: 10px; cursor: pointer; vertical-align: bottom; font-weight: 600;"]').contains('클래스 공개').should('exist')
-
-    cy.get('.clicked > span').contains('수강생').should('exist')
-    cy.get('.lk-tabs > :nth-child(2) > span').contains('클래스 설정').should('exist')
-    cy.get('.lk-tabs > :nth-child(3) > span').contains('강의관리').should('exist')
-    cy.get('.lk-tabs > :nth-child(4) > span').contains('결제설정').should('exist')
-    cy.get('.lk-tabs > :nth-child(5) > span').contains('추가정보').should('exist')
-
-    cy.get('.btn-email-list > .btn-label > span').contains('발송 이력 확인').should('exist')
-    cy.get('.btn-invite > .btn-label > span').contains('초대하기').should('exist')
+    if( i == 0 ){
+      cy.get('.btn-move > .btn-label > span').contains('클래스로 돌아가기').should('exist')
+      cy.get('[style="font-size: 16px; color: rgb(17, 42, 68); margin-right: 10px; cursor: pointer; vertical-align: bottom; font-weight: 600;"]').contains('클래스 공개').should('exist')
+      cy.get('.clicked > span').contains('수강생').should('exist')
+      cy.get('.lk-tabs > :nth-child(2) > span').contains('클래스 설정').should('exist')
+      cy.get('.lk-tabs > :nth-child(3) > span').contains('강의관리').should('exist')
+      cy.get('.lk-tabs > :nth-child(4) > span').contains('결제설정').should('exist')
+      cy.get('.lk-tabs > :nth-child(5) > span').contains('추가정보').should('exist')
+      cy.get('.btn-email-list > .btn-label > span').contains('발송 이력 확인').should('exist')
+      cy.get('.btn-invite > .btn-label > span').contains('초대하기').should('exist')
+    }else{
+      cy.get('.btn-move > .btn-label > span').contains('Back to class').should('exist')
+      cy.get('[style="font-size: 16px; color: rgb(17, 42, 68); margin-right: 10px; cursor: pointer; vertical-align: bottom; font-weight: 600;"]').contains('클래스 공개').should('exist')
+      cy.get('.clicked > span').contains('수강생').should('exist')
+      cy.get('.lk-tabs > :nth-child(2) > span').contains('Class Setting').should('exist')
+      cy.get('.lk-tabs > :nth-child(3) > span').contains('강의관리').should('exist')
+      cy.get('.lk-tabs > :nth-child(4) > span').contains('결제설정').should('exist')
+      cy.get('.lk-tabs > :nth-child(5) > span').contains('추가정보').should('exist')
+      cy.get('.btn-email-list > .btn-label > span').contains('발송 이력 확인').should('exist')
+      cy.get('.btn-invite > .btn-label > span').contains('초대하기').should('exist')
+    }
     cy.wait(500)
+
     // 이메일 초대
     cy.get('.new-tag').type('ruoghks@gmail.com')
     cy.get('.new-tag').type('{enter}')
@@ -82,15 +112,24 @@ describe('empty spec', () => {
     cy.wait(500)
     //강사 추가 
     cy.get(':nth-child(2) > :nth-child(1) > .manage-facilitator-item').click()
-
-    cy.get('.lk-radio-group > .clicked > span').contains('이메일초대').should('exist')
-    cy.get('.lk-radio-group > :nth-child(2) > span').contains('기존 강사에서 추가').should('exist')
-    cy.get('.btn-next').contains('다음').should('exist')
+    if( i == 0 ){
+      cy.get('.lk-radio-group > .clicked > span').contains('이메일초대').should('exist')
+      cy.get('.lk-radio-group > :nth-child(2) > span').contains('기존 강사에서 추가').should('exist')
+      cy.get('.btn-next').contains('다음').should('exist')
+    }else{
+      cy.get('.lk-radio-group > .clicked > span').contains('이메일초대').should('exist')
+      cy.get('.lk-radio-group > :nth-child(2) > span').contains('기존 강사에서 추가').should('exist')
+      cy.get('.btn-next').contains('다음').should('exist')
+    }
     cy.wait(500)
-    cy.get('.lk-input-inner > input').type('ruoghks@kakao.com')
+    cy.get('.lk-input-inner > input').type('cekajif719@yubua.com')
     cy.get('.btn-next').click()
     cy.wait(500)
-    cy.get('.btn-add-user > .btn-label > span').contains('강사로 추가하기').should('exist')
+    if( i == 0 ){
+      cy.get('.btn-add-user > .btn-label > span').contains('강사로 추가하기').should('exist')
+    }else{
+      cy.get('.btn-add-user > .btn-label > span').contains('강사로 추가하기').should('exist')
+    }
     cy.get('.btn-add-user > .btn-label > span').click()
     cy.wait(500)
     //수강생 목록 보기
@@ -102,8 +141,13 @@ describe('empty spec', () => {
     //강사 삭제
     cy.get(':nth-child(2) > :nth-child(1) > .manage-facilitator-item > .edit-box > .lk-icon > img').click()
     cy.wait(500)
-    cy.get('.button-container > .primary > .btn-label > span').contains('저장하기').should('exist')
-    cy.get('.delete > .btn-label > span').contains('초대취소').should('exist')
+    if( i == 0 ){
+      cy.get('.button-container > .primary > .btn-label > span').contains('저장하기').should('exist')
+      cy.get('.delete > .btn-label > span').contains('초대취소').should('exist')
+    }else{
+      cy.get('.button-container > .primary > .btn-label > span').contains('저장하기').should('exist')
+      cy.get('.delete > .btn-label > span').contains('초대취소').should('exist')
+    }
     cy.wait(500)
     cy.get('.delete > .btn-label > span').click()
     cy.get('.lk-modal-footer > .primary > .btn-label > span').click()
@@ -123,16 +167,27 @@ describe('empty spec', () => {
     cy.get('.lk-tabs > :nth-child(2) > span').click()
     cy.wait(500)
     //check
-    cy.get('.btn > .btn-label > span').contains('클래스 이미지 변경하기').should('exist')
-    cy.get('.mobile-chx > .chk-label').contains('썸네일 이미지에 텍스트 표시').should('exist')
-    cy.get('.editor-check-container > .lk-checkbox > .chk-label').contains('에디터로 꾸미기').should('exist')
-
-    cy.get(':nth-child(1) > :nth-child(1) > .chk-label').contains('모집 정원').should('exist')
-    cy.get(':nth-child(2) > .checks > .chk-label').contains('운영자 선정').should('exist')
-    cy.get('.row-inner-white > :nth-child(3) > .checks > .chk-label').contains('모집 기간').should('exist')
-    cy.get(':nth-child(4) > .row-inner-item-label').contains('수강기간').should('exist')
-    cy.get(':nth-child(5) > .row-inner-item-label').contains('카테고리').should('exist')
-    cy.get('.btn-submit > .btn-label > span').contains('변경사항 저장하기').should('exist')
+    if( i == 0 ){
+      cy.get('.btn > .btn-label > span').contains('클래스 이미지 변경하기').should('exist')
+      cy.get('.mobile-chx > .chk-label').contains('썸네일 이미지에 텍스트 표시').should('exist')
+      cy.get('.editor-check-container > .lk-checkbox > .chk-label').contains('에디터로 꾸미기').should('exist')
+      cy.get(':nth-child(1) > :nth-child(1) > .chk-label').contains('모집 정원').should('exist')
+      cy.get(':nth-child(2) > .checks > .chk-label').contains('운영자 선정').should('exist')
+      cy.get('.row-inner-white > :nth-child(3) > .checks > .chk-label').contains('모집 기간').should('exist')
+      cy.get(':nth-child(4) > .row-inner-item-label').contains('수강기간').should('exist')
+      cy.get(':nth-child(5) > .row-inner-item-label').contains('카테고리').should('exist')
+      cy.get('.btn-submit > .btn-label > span').contains('변경사항 저장하기').should('exist')
+    }else{
+      cy.get('.btn > .btn-label > span').contains('클래스 이미지 변경하기').should('exist')
+      cy.get('.mobile-chx > .chk-label').contains('썸네일 이미지에 텍스트 표시').should('exist')
+      cy.get('.editor-check-container > .lk-checkbox > .chk-label').contains('에디터로 꾸미기').should('exist')
+      cy.get(':nth-child(1) > :nth-child(1) > .chk-label').contains('모집 정원').should('exist')
+      cy.get(':nth-child(2) > .checks > .chk-label').contains('운영자 선정').should('exist')
+      cy.get('.row-inner-white > :nth-child(3) > .checks > .chk-label').contains('모집 기간').should('exist')
+      cy.get(':nth-child(4) > .row-inner-item-label').contains('수강기간').should('exist')
+      cy.get(':nth-child(5) > .row-inner-item-label').contains('카테고리').should('exist')
+      cy.get('.btn-submit > .btn-label > span').contains('변경사항 저장하기').should('exist')
+    }
 
     // 제목 변경
     cy.get('.lk-input-inner > input').clear()
@@ -168,8 +223,13 @@ describe('empty spec', () => {
     cy.wait(500)
 
     //check
-    cy.get('.detail-wrapper > :nth-child(1)').contains('100명 모집').should('exist')
-    cy.get('.detail-wrapper > :nth-child(3)').contains('1개월 수강').should('exist')
+    if( i == 0 ){
+      cy.get('.detail-wrapper > :nth-child(1)').contains('100명 모집').should('exist')
+      cy.get('.detail-wrapper > :nth-child(3)').contains('1개월 수강').should('exist')
+    }else{
+      cy.get('.detail-wrapper > :nth-child(1)').contains('Enrollment limit: 100').should('exist')
+      cy.get('.detail-wrapper > :nth-child(3)').contains('Class period: 1 month').should('exist')
+    }
     cy.wait(500)
     //원복
     cy.contains('클래스 관리').click()
@@ -236,11 +296,19 @@ describe('empty spec', () => {
     cy.wait(1000)
 
     //check
-    cy.get('.discount-promotion.mb6').contains('프로모션').should('exist')
-    cy.get(':nth-child(3) > .discount').contains('현재 판매가 1,000원').should('exist')
-    cy.get('.flex > .discount-promotion').contains('5개월 무이자 할부 시').should('exist')
-    cy.get('.discount.mb6').contains('정가 10,000원 90% 할인').should('exist')
-    cy.get('.price').contains('월 200원').should('exist')
+    if( i == 0 ){
+      cy.get('.discount-promotion.mb6').contains('프로모션').should('exist')
+      cy.get(':nth-child(3) > .discount').contains('현재 판매가 1,000원').should('exist')
+      cy.get('.flex > .discount-promotion').contains('5개월 무이자 할부 시').should('exist')
+      cy.get('.discount.mb6').contains('정가 10,000원 90% 할인').should('exist')
+      cy.get('.price').contains('월 200원').should('exist')
+    }else{
+      cy.get('.discount-promotion.mb6').contains('Promotion').should('exist')
+      cy.get(':nth-child(3) > .discount').contains('현재 판매가 1,000원').should('exist')
+      cy.get('.flex > .discount-promotion').contains('5개월 무이자 할부 시').should('exist')
+      cy.get('.discount.mb6').contains('List price 10,000원 90% discount').should('exist')
+      cy.get('.price').contains('월 200원').should('exist')
+    }
     cy.wait(2000)
 
     //원복
@@ -278,8 +346,13 @@ describe('empty spec', () => {
     cy.get(':nth-child(5) > span').click()
     cy.wait(500)
     
-    cy.get('.manage-question-item > .lk-button > .btn-label > span').contains('입력 정보 추가').should('exist')
-    cy.get('.chk-label').contains('추가 정보 사용').should('exist')
+    if( i == 0 ){
+      cy.get('.manage-question-item > .lk-button > .btn-label > span').contains('입력 정보 추가').should('exist')
+      cy.get('.chk-label').contains('추가 정보 사용').should('exist')
+    }else{
+      cy.get('.manage-question-item > .lk-button > .btn-label > span').contains('입력 정보 추가').should('exist')
+      cy.get('.chk-label').contains('추가 정보 사용').should('exist')
+    }
     cy.wait(500)
 
     cy.get('.manage-question-item > .lk-button > .btn-label > span').click()
@@ -316,8 +389,11 @@ describe('empty spec', () => {
     cy.get('.pay > .btn-label > span').click()
     cy.wait(500)
 
-    cy.get('[style="width: 100%;"] > :nth-child(1) > div > .form_label').contains('(필수) 추가정보 입력').should('exist')
-
+    if( i == 0 ){
+      cy.get('[style="width: 100%;"] > :nth-child(1) > div > .form_label').contains('(필수) 추가정보 입력').should('exist')
+    }else{
+      cy.get('[style="width: 100%;"] > :nth-child(1) > div > .form_label').contains('(필수) 추가정보 입력').should('exist')
+    }
     //============================================================ 
     //원복
 
@@ -358,7 +434,7 @@ describe('empty spec', () => {
     cy.get('.btn-move > .btn-label > span').click()
   })
 
-  it.only('6-6. class update(강의 관리- 라이브 미팅 추가))', () => {
+  it('6-6. class update(강의 관리- 라이브 미팅 추가))', () => {
     //프로그램 이동
     cy.get(':nth-child(2) > .header-menu-item-btn').click()
     cy.wait(500)
@@ -376,14 +452,23 @@ describe('empty spec', () => {
     cy.get('.add').click()
     cy.wait(100)
 
-    cy.contains('라이브 미팅').should('exist')
-    cy.contains('동영상 강의').should('exist')
-    cy.contains('오프라인 모임').should('exist')
-    cy.contains('디지털 상품').should('exist')
-
-    cy.contains('줌').should('exist')
-    cy.contains('구글미트').should('exist')
-    cy.contains('기타').should('exist')
+    if( i == 0 ){
+      cy.contains('라이브 미팅').should('exist')
+      cy.contains('동영상 강의').should('exist')
+      cy.contains('오프라인 모임').should('exist')
+      cy.contains('디지털 상품').should('exist')
+      cy.contains('줌').should('exist')
+      cy.contains('구글미트').should('exist')
+      cy.contains('기타').should('exist')
+    }else{
+      cy.contains('라이브 미팅').should('exist')
+      cy.contains('동영상 강의').should('exist')
+      cy.contains('오프라인 모임').should('exist')
+      cy.contains('디지털 상품').should('exist')
+      cy.contains('줌').should('exist')
+      cy.contains('구글미트').should('exist')
+      cy.contains('기타').should('exist')
+    }
     cy.wait(500)
 
     // 강의 제목 입력
@@ -420,7 +505,7 @@ describe('empty spec', () => {
   })
 
 
-  it.only('6-7. class update(강의 관리- 라이브 미팅 추가))', () => {
+  it('6-7. class update(강의 관리- 라이브 미팅 추가))', () => {
     //프로그램 이동
     cy.get(':nth-child(2) > .header-menu-item-btn').click()
     cy.wait(500)
@@ -440,11 +525,16 @@ describe('empty spec', () => {
 
     //동영상 강의 클릭
     cy.get('[style=""] > span').click()
-
-    cy.contains('업로드').should('exist')
-    cy.contains('유투브').should('exist')
-    cy.contains('비메오').should('exist')
-
+    if( i == 0 ){
+      cy.contains('업로드').should('exist')
+      cy.contains('유투브').should('exist')
+      cy.contains('비메오').should('exist')
+    }else{
+      cy.contains('업로드').should('exist')
+      cy.contains('유투브').should('exist')
+      cy.contains('비메오').should('exist')
+    }
+    
     //동영상 강의 제목 추가
     cy.get('[data-testid="-input"]').click()
 
